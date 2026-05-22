9210eb84751ca52a74779504a0d635cfcb98901f
Ramin293 <erikgan3@gmail.com>
2026-05-22 16:26:07 +0500

    add child Expense class

---
+++ models/expense.py
@@ -0,0 +1,16 @@
+import Transaction
+
+class Expense(Transaction):
+    def __init__(self, transaction_id, amount, date, description, category):
+        super().__init__(transaction_id, amount, date, description)
+        self.category = category
+    
+    def apply(self, balance):
+        return self.amount - balance
+    
+    def to_dict(self):
+        data = super().to_dict()
+        data["type"] = "expense"
+        data["category"] = self.category
+        return data
+    
