9b90dbc129a8594f5b46e9af50dcc004f8264791
cuchj0 <arai.uakit@mail.ru>
2026-05-24 19:09:35 +0500

    fixing

--- utils/console_ui.py
+++ utils/console_ui.py
@@ -1,9 +1,25 @@
 from colorama import Fore, Back, Style
 
-def print_header(manager = None):
+def clear_screen():
+    print("\n" * 2)
+
+def print_header(manager=None):
     print("=" * 50)
     print("            PERSONAL FINANCE TRACKER")
     print("=" * 50)
+
+    if manager is not None:
+        balance = manager.calculate_balance()
+        
+        if balance > 0:
+            color = Fore.GREEN
+        elif balance < 0:
+            color = Fore.RED
+        else:
+            color = Fore.YELLOW
+
+        print(f"Current balance: {color}{balance} tg{Style.RESET_ALL}")
+        print("=" * 50)
 
 def print_menu():
     print("\n             Chose an option:")
@@ -21,22 +37,3 @@ def print_menu():
 def pause():
     input("\n Press enter to continue...")
 
-def clear_screen():
-    print("\n" * 2)
-
-def print_header(manager=None):
-    print("=" * 50)
-    print("            PERSONAL FINANCE TRACKER")
-    print("=" * 50)
-
-    if manager is not None:
-        balance = manager.calculate_balance()
-        if balance > 0:
-            color = Fore.GREEN
-        elif balance < 0:
-            color = Fore.RED
-        else:
-            color = Fore.YELLOW
-
-        print(f"Current balance: {color}{balance} tg{Style.RESET_ALL}")
-        print("=" * 50)
